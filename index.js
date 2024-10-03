28e37f895db8297a4ce9b7a9690864ee7ae22b82
Ryan Ragasa <rragasa23@vt.edu>
2024-10-03 09:20:36 -0400

    feat: included mongodb atlas db instead of in memory db

--- index.js
+++ index.js
@@ -1,72 +1,62 @@
 require("dotenv").config();
 const express = require("express");
-const cors = require("cors");
 const app = express();
+const cors = require("cors");
+
+const { MongoClient } = require("mongodb");
+const client = new MongoClient(process.env.MONGO_URI);
+const db = client.db("fcc-urlshortener");
+const urls = db.collection("urls");
+
 const bodyParser = require("body-parser");
 const nanoid = require("nanoid");
 const dns = require("dns");
-const url = require("url");
+const urlparser = require("url");
+
+// In-memory database for short Urls
+// const urls = {};
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
-
-// In-memory database for short Urls
-const urls = {};
-
-// Handle form data for post requests
+app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
 app.use(cors());
-
 app.use("/public", express.static(`${process.cwd()}/public`));
-
 app.get("/", function (req, res) {
   res.sendFile(process.cwd() + "/views/index.html");
 });
 
+// Process form data
 app.post("/api/shorturl", (req, res) => {
-  const url = req.body.url;
+  const url = new URL(req.body.url);
+  const hostname = url.hostname;
 
-  let hostname;
-
-  try {
-    // Use the URL constructor to parse the URL
-    const parsedUrl = new URL(url);
-    hostname = parsedUrl.hostname;
-  } catch (error) {
-    // If the URL is invalid, return an error
-    return res.json({ error: "invalid url" });
-  }
-
-  if (!hostname) {
-    return res.json({ error: "invalid url" });
-  }
-
-  dns.lookup(hostname, (err, address, family) => {
-    if (err) {
+  const dnslookup = dns.lookup(hostname, async (err, address, family) => {
+    if (!address) {
       console.log(err);
       return res.json({ error: "invalid url" });
+    } else {
+      // Generate the short code
+      const shortCode = nanoid.nanoid(7);
+
+      // Store in database
+      const urlDoc = {
+        url: req.body.url,
+        short_url: shortCode,
+      };
+      const result = await urls.insertOne(urlDoc);
+
+      // Respond with the original URL and the short code
+      res.json({ original_url: url, short_url: shortCode });
     }
-
-    // Generate the short code and store the URL
-    const shortCode = nanoid.nanoid(7);
-    urls[shortCode] = url;
-
-    // Respond with the original URL and the short code
-    res.json({ original_url: url, short_url: shortCode });
   });
 });
 
-// Handle URL Shortener
-app.get("/api/shorturl/:shortCode", (req, res) => {
+// Handle URL Shortener and allow redirect
+app.get("/api/shorturl/:shortCode", async (req, res) => {
   const shortCode = req.params.shortCode;
-  const originalUrl = urls[shortCode];
-
-  if (originalUrl) {
-    res.redirect(originalUrl);
-  } else {
-    res.status(404).json({ error: "invalid url" });
-  }
+  const originalUrl = await urls.findOne({ short_url: shortCode });
+  res.redirect(originalUrl.url);
 });
 
 app.listen(port, function () {
